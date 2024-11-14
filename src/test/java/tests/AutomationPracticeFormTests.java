dbff13a238034dbaa9dba633d7fb918772d0b970
Ann Kornilova <Kirbri@yandex.ru>
2024-11-14 14:01:26 +0300

    12. Fix for size browser

--- src/test/java/tests/AutomationPracticeFormTests.java
+++ src/test/java/tests/AutomationPracticeFormTests.java
@@ -25,23 +25,23 @@ public class AutomationPracticeFormTests extends TestBase{
         });
 
         step("Заполняем данные формы", () -> {
-                    $("#firstName").setValue("Olga");
-                    $("#lastName").setValue("Ivanova");
-                    $("#userEmail").setValue("dfsdj@dsfsd.dsfsd");
-                    $("#genterWrapper").$(byText("Other")).click();
-                    $("#userNumber").setValue("1234567890");
-                    $("#subjectsInput").setValue("Arts").pressEnter();
-                    $("#hobbiesWrapper").$(byText("Sports")).click();
-                    $("#hobbiesWrapper").$(byText("Reading")).click();
-                    $("#hobbiesWrapper").$(byText("Music")).click();
-                    $("#uploadPicture").uploadFromClasspath("image/reception.png");
-                    $("#currentAddress").setValue("Some Current Address");
+                    $("#firstName").scrollTo().setValue("Olga");
+                    $("#lastName").scrollTo().setValue("Ivanova");
+                    $("#userEmail").scrollTo().setValue("dfsdj@dsfsd.dsfsd");
+                    $("#genterWrapper").scrollTo().$(byText("Other")).click();
+                    $("#userNumber").scrollTo().setValue("1234567890");
+                    $("#subjectsInput").scrollTo().setValue("Arts").pressEnter();
+                    $("#hobbiesWrapper").scrollTo().$(byText("Sports")).click();
+                    $("#hobbiesWrapper").scrollTo().$(byText("Reading")).click();
+                    $("#hobbiesWrapper").scrollTo().$(byText("Music")).click();
+                    $("#uploadPicture").scrollTo().uploadFromClasspath("image/reception.png");
+                    $("#currentAddress").scrollTo().setValue("Some Current Address");
                     $("#state").scrollTo().click();
-                    $("#stateCity-wrapper").$(byText("Rajasthan")).click();
-                    $("#city").click();
-                    $("#stateCity-wrapper").$(byText("Jaiselmer")).click();
+                    $("#stateCity-wrapper").scrollTo().$(byText("Rajasthan")).click();
+                    $("#city").scrollTo().click();
+                    $("#stateCity-wrapper").scrollTo().$(byText("Jaiselmer")).click();
 
-                    $("#dateOfBirthInput").click();
+                    $("#dateOfBirthInput").scrollTo().click();
                     $(".react-datepicker__year-select").sendKeys("1990");
                     $(".react-datepicker__month-select").sendKeys("November");
                     $(".react-datepicker__day--015").click();
@@ -51,19 +51,19 @@ public class AutomationPracticeFormTests extends TestBase{
 
         step("Проверяем корректность данных", () -> {
                     $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-                    $(".modal-body").shouldHave(text("Olga Ivanova"));
-                    $(".modal-body").shouldHave(text("dfsdj@dsfsd.dsfsd"));
-                    $(".modal-body").shouldHave(text("Other"));
-                    $(".modal-body").shouldHave(text("1234567890"));
-                    $(".modal-body").shouldHave(text("15 November,1990"));
-                    $(".modal-body").shouldHave(text("Arts"));
-                    $(".modal-body").shouldHave(text("Sports, Reading, Music"));
-                    $(".modal-body").shouldHave(text("reception.png"));
-                    $(".modal-body").shouldHave(text("Some Current Address"));
-                    $(".modal-body").shouldHave(text("Rajasthan Jaiselmer"));
+                    $(".modal-body").scrollTo().shouldHave(text("Olga Ivanova"));
+                    $(".modal-body").scrollTo().shouldHave(text("dfsdj@dsfsd.dsfsd"));
+                    $(".modal-body").scrollTo().shouldHave(text("Other"));
+                    $(".modal-body").scrollTo().shouldHave(text("1234567890"));
+                    $(".modal-body").scrollTo().shouldHave(text("15 November,1990"));
+                    $(".modal-body").scrollTo().shouldHave(text("Arts"));
+                    $(".modal-body").scrollTo().shouldHave(text("Sports, Reading, Music"));
+                    $(".modal-body").scrollTo().shouldHave(text("reception.png"));
+                    $(".modal-body").scrollTo().shouldHave(text("Some Current Address"));
+                    $(".modal-body").scrollTo().shouldHave(text("Rajasthan Jaiselmer"));
 
                 });
         step("Закрываем форму с регистрационными данными", () ->
-                $("#closeLargeModal").click());
+                $("#closeLargeModal").scrollTo().click());
     }
 }
