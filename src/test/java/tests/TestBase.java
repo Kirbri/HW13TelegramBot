3590fb9d308d25a9fdcbf9fd45dc77d86db4a30b
Ann Kornilova <Kirbri@yandex.ru>
2024-11-16 17:15:49 +0300

    12. Add check logic for chrome browser in allure

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -60,8 +60,8 @@ public class TestBase {
         Attach.screenshotAs("Last screenshot");
         if (browser.equals("chrome")) {
             Attach.pageSource();
+            Attach.browserConsoleLogs();
         }
-        Attach.browserConsoleLogs();
         Attach.addVideo();
     }
 }
